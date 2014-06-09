97c127705e7c05f858b864d13dbcc82f49c764c9
gotealeaf2014 <gotealeaf2014@users.noreply.github.com>
2014-06-09 08:20:41 +0000

    arrays completed

---
+++ 06_arrays/05_exercise.rb
@@ -0,0 +1,3 @@
+puts "value of a is e"
+puts "value of b is T"
+puts "value of c is A"
---
+++ 06_arrays/07_exercise.rb
@@ -0,0 +1,7 @@
+array1 = [2, 5, 7, 3]
+array2 = []
+  array1.each do |n|
+  	array2 << n + 2
+  end
+p array1
+p array2
