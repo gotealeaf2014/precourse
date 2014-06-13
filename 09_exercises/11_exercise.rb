2f0a274c3eb254f3ecfd3933f6190c884b3f506f
gotealeaf2014 <gotealeaf2014@users.noreply.github.com>
2014-06-13 07:25:32 +0000

    Precourse complete

---
+++ 09_exercises/11_exercise.rb
@@ -0,0 +1,1 @@
+puts "I like array of hashes because it resembles flat json."
---
+++ 09_exercises/13_exercise.rb
@@ -0,0 +1,4 @@
+contacts = {"Joe Smith"=>["joe@email.com", "123 Main st.", "555-123-4567"], "Sally Johnson"=>["sally@email.com", "404 Not Found Dr.", "123-234-3454"]}
+
+p contacts["Joe Smith"][0]
+p contacts["Sally Johnson"][2]
